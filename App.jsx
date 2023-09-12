dfaed61d6a1042b528b57ac85e7fad311b63bab9
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-09-12 07:48:11 +0200

    add a GetInfo button which returns the profile information of the logged user

--- App.jsx
+++ App.jsx
@@ -2,8 +2,10 @@ import React, { useEffect } from "react"
 
 export default function App()
 {
+    const [token, SetToken] = React.useState("")
     const clientID = "3ba0d9e71d40432dad224aacbefec132"
     const clientSecret = "e5aed49d8f7549aa8168fc0ede9c0c9a"
+
     const [accessToken, setAccessToken] = React.useState("")
     useEffect(() => {
     //API Access Token
@@ -15,28 +17,76 @@ export default function App()
     fetch('https://accounts.spotify.com/api/token', authParams)
     .then(res => res.json())
     .then(data => setAccessToken(data.access_token))
+
     }, [])
 
     const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
     const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5173/";
     const SPACE_DELIMITER = "%20";
     const SCOPES = [
+      "user-read-email",
+      "user-read-private",
     "user-read-currently-playing",
     "user-read-playback-state",
     "playlist-read-private",
     ];
     const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
     
+    const getReturnedParamsFromSpotifyAuth = (hash) => {
+        const stringAfterHashtag = hash.substring(1); //removes first letter
+        const paramsInUrl = stringAfterHashtag.split("&"); //splits on every &
+        const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+          console.log(currentValue);
+          const [key, value] = currentValue.split("=");
+          accumulater[key] = value;
+          return accumulater;
+        }, {});
+      
+        return paramsSplitUp;
+      };
+
+      useEffect(() => {
+        if (window.location.hash) {
+          const { access_token, expires_in, token_type } =
+            getReturnedParamsFromSpotifyAuth(window.location.hash);
     
+          localStorage.clear();
+    
+          localStorage.setItem("accessToken", access_token);
+          localStorage.setItem("tokenType", token_type);
+          localStorage.setItem("expiresIn", expires_in);
+
+        
+        }
+      });
+
         const handleLogin = () => {
             window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${clientID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
         }
-        
+    
+        const getInfo = () => {
+          let oneUseToken = ""
+          if(localStorage.getItem("accessToken"))
+          { 
+            SetToken(localStorage.getItem("accessToken"))
+            oneUseToken = localStorage.getItem("accessToken")
+          }
+          var authParams2 = {
+
+            method: "GET",
+            headers: { Authorization: `Bearer ${oneUseToken}` }
+          }
+         
+          fetch('https://api.spotify.com/v1/me  ', authParams2)
+          .then(res => res.json())
+          .then(data => console.log(data))
+        }
 
     return(
         <div>
             <h1>hi</h1>
             <button onClick={handleLogin}> Login</button>
+            <button onClick={getInfo}> Get Info</button>
         </div>
     )
 }
