7c2a24388bf4aa19cb2cff75bb6f8731853c7244
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-09-22 09:46:29 +0200

    outsource the MainPage in a seperate Component

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import Song from "./Components/Song"
 import GuessPage from "./Components/GuessPage"
 import Timer from "./Components/Timer"
 import Navbar from "./Components/Navbar"
-
+import MainPage from "./Components/MainPage"
 export default function App()
 {
     const [token, SetToken] = React.useState("")
@@ -177,24 +177,9 @@ export default function App()
          url = {profilePicture.url}/>     
         <main className="flex flex-col items-center">     
               
-              {!showGuessPage && <> <div className="flex gap-24 mt-8"><img className="opacity-30 w-10 h-10" src="Images/ReactLogo.png"></img>
-              <img className="opacity-30 w-10 h-10" src="Images/SpotifyLogo.png"></img></div>
-                <h1 className="text-white mt-6"> This project was made with React and SpotifyAPI. 
-                </h1>
-                <div className="mt-10 p-8 rounded-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
-                "> <h1 className="text-center mb-5 text-white text-4xl font-display">How to play:</h1>
-                <ul className="text-white text-lg">
-                  <li>1. Click the Login button and login with your spotify account</li>
-                  <li>2. Click the Start button</li>
-                  <li>3. You have 15 seconds to guess the random song</li>
-                  <li>4. Type the name of the song in the input field below</li>
-                  <li>5. Click the Submit button</li></ul></div>
-                  
-                  <button className="font-display  text-2xl bg-blue-400 py-4 px-16
-               rounded-xl mt-6 border-red-800 cursor-pointer hover:text-white"
-                onClick={getInfo}> Start</button></>}
-              {showText && <h2 className="text-white"> Please login first! </h2>}
-         
+        <MainPage showGuessPage={showGuessPage}
+        showText={showText}
+        getInfo={getInfo}/>   
 
               {showGuessPage && !timeRanOut ? <h3 className="font-display  text-2xl
                bg-blue-400 py-4 px-16 rounded-xl mt-6 "><Timer updateTimer={updateTimer} /></h3>: null}
--- Components/MainPage.jsx
+++ Components/MainPage.jsx
@@ -1,7 +1,25 @@
 import React from "react"
 
-export default function MainPage(){
+export default function MainPage(props){
     return(
-        <></>
+        <>
+        {!props.showGuessPage && <> <div className="flex gap-24 mt-8"><img className="opacity-30 w-10 h-10" src="Images/ReactLogo.png"></img>
+              <img className="opacity-30 w-10 h-10" src="Images/SpotifyLogo.png"></img></div>
+                <h1 className="text-white mt-6"> This project was made with React and SpotifyAPI. 
+                </h1>
+                <div className="mt-10 p-8 rounded-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
+                "> <h1 className="text-center mb-5 text-white text-4xl font-display">How to play:</h1>
+                <ul className="text-white text-lg">
+                  <li>1. Click the Login button and login with your spotify account</li>
+                  <li>2. Click the Start button</li>
+                  <li>3. You have 15 seconds to guess the random song</li>
+                  <li>4. Type the name of the song in the input field below</li>
+                  <li>5. Click the Submit button</li></ul></div>
+                  
+                  <button className="font-display  text-2xl bg-blue-400 py-4 px-16
+               rounded-xl mt-6 border-red-800 cursor-pointer hover:text-white"
+                onClick={props.getInfo}> Start</button></>}
+              {props.showText && <h2 className="text-white"> Please login first! </h2>}
+        </>
     )
 }
